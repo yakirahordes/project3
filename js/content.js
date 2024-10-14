b922d188ccf37b47c7351c70ee2657df408af0de
rotemn2006 <rotemneugeboren@hilma.tech>
2024-10-14 11:23:14 +0300

    table

--- js/content.js
+++ js/content.js
@@ -1,63 +1,74 @@
-class  contact {
-    constructor ( name, phoneNumber) {
-        this. name =  name;
+
+class contact {
+    constructor(name, phoneNumber) {
+        this.name = name;
         this.phoneNumber = phoneNumber;
-       
+
     }
 }
 
 
-
-
-function getContact(){
-    let currentUserName= JSON.parse(localStorage.getItem("currentUser"));
+function getContact() {
+    let currentUserName = JSON.parse(localStorage.getItem("currentUser"));
     console.log('currentUser: ', currentUserName);
-    let usersArray= JSON.parse(localStorage.getItem("usersArray"));
+    let usersArray = JSON.parse(localStorage.getItem("usersArray"));
     console.log('usersArray: ', usersArray);
-    let rightUser =usersArray.find(user => {
-        if( user.username===currentUserName){
+    let rightUser = usersArray.find(user => {
+        if (user.username === currentUserName) {
             return user;
         }
     })
-   return rightUser;
+    return rightUser;
 }
 
 
+let userObj = getContact();
 
-function setContact(userObj){
-    let currentUserName= JSON.parse(localStorage.getItem("currentUser"));
-    let usersArray= JSON.parse(localStorage.getItem("usersArray"));
-    console.log('usersArray: ', usersArray);
-    let newUsersArray =usersArray.map(user => {
-        if( user.username===currentUserName){
+function saveNewContact(event) {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    const newContact = new contact(name, phoneNumber);
+    userObj.content.push(newContact);
+    setContact(userObj);
+
+}
+
+function setContact(userObj) {
+    let currentUserName = JSON.parse(localStorage.getItem("currentUser"));
+    let usersArray = JSON.parse(localStorage.getItem("usersArray"));
+    let newUsersArray = usersArray.map(user => {
+        if (user.username === currentUserName) {
             return userObj;
         }
         return user;
     })
+    localStorage.setItem("usersArray", JSON.stringify(newUsersArray));
+    displayContact();
+}
 
-     localStorage.setItem("usersArray",JSON.stringify(newUsersArray));
 
+
+function displayContact() {
+     
+    const contactsArr = userObj.content;
+    console.log('contactsArr: ', contactsArr);
+    const wholeTable=document.getElementById("contactsTable");
+    const table = document.getElementById("contacts");
+    wholeTable.appendChild(table);
+    table.removeChild;
+
+    for (let i = 0; i < contactsArr.length; i++) {
+        console.log("hi");
+        console.log('contactsArr[i].name: ', contactsArr[i].name);
+        const row = document.createElement("tr");
+        const namesItems = document.createElement("td");
+        const phoneItems = document.createElement("td");
+        namesItems.textContent = contactsArr[i].name;
+        phoneItems.textContent = contactsArr[i].phoneNumber;
+        row.appendChild(namesItems);
+        row.appendChild(phoneItems);
+
+        table.appendChild(row);
     }
-
-
-    
-let userObj=getContact();
-
-function saveNewContact(event){
-    event.preventDefault();
-    console.log("hi");
-    let name=document.getElementById("name").value;
-    let phoneNumber=document.getElementById("phoneNumber").value;
-
-    const newContact =new contact(name, phoneNumber);
-
-    console.log(' newContent: ',  newContact);
-   
-   
-    
-    userObj.content.push(newContact);
-    console.log('contactArray.content: ', userObj.content);
-    console.log('contactArray: ', userObj);
-    setContact(userObj);
-    
 }
