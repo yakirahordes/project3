f974ae4d8829e2d85ba64a42ee48ed69e60b06b7
yakirahordes <yakirahordes@hilma.tech>
2024-10-14 08:48:39 +0300

    6

--- js/content.js
+++ js/content.js
@@ -7,47 +7,87 @@ class  contact {
 }
 
 
-
-
-function getContact(){
-    let currentUserName= JSON.parse(localStorage.getItem("currentUser"));
-    let rightUser =usersArray.find(user => {
-        console.log('usersArray: ', usersArray);
+function getContact() {
+    let currentUserName = JSON.parse(localStorage.getItem("currentUser"));
+    let usersArray = JSON.parse(localStorage.getItem("usersArray"));
+    let rightUser = usersArray.find(user => {
         if( user.username===currentUserName){
-            return user.content;
+            return user;
         }
     })
-
-}
-console.log(getContact());
-
-
-function getCurrentUser(){
-    return  JSON.parse(localStorage.getItem("currentUser"));
+    console.log('rightUser: ', rightUser);
+   return rightUser;
+   
 }
 
-function setContact(contactArray){
-const cur = getCurrentUser();
-cur.content = contactArray;
-     localStorage.setItem("currentUser ",JSON.stringify(cur));
 
-    
-    }
-    
-let contactArray=getContact();
-function saveNewContact(event){
-    event.preventDefault();
-    console.log("hi");
-    let name=document.getElementById("name").value;
-    let phoneNumber=document.getElementById("phoneNumber").value;
 
-    const newContact =new contact(name, phoneNumber);
-    console.log(' newContent: ',  newContact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getContact(){
+//     let currentUserName= JSON.parse(localStorage.getItem("currentUser"));
+//     let rightUser =usersArray.find(user => {
+//         console.log('usersArray: ', usersArray);
+//         if( user.username===currentUserName){
+//             return user.content;
+//         }
+//     })
+
+// }
+// console.log(getContact());
+
+
+// function getCurrentUser(){
+//     return  JSON.parse(localStorage.getItem("currentUser"));
+// }
+
+// function setContact(contactArray){
+// const cur = getCurrentUser();
+// cur.content = contactArray;
+//      localStorage.setItem("currentUser ",JSON.stringify(cur));
+//     }
+    
+// let contactArray=getContact();
+// function saveNewContact(event){
+//     event.preventDefault();
+//     console.log("hi");
+//     let name=document.getElementById("name").value;
+//     let phoneNumber=document.getElementById("phoneNumber").value;
+
+//     const newContact =new contact(name, phoneNumber);
+//     console.log(' newContent: ',  newContact);
    
    
     
-    contactArray.push(newContact);
-    console.log('contactArray: ', contactArray);
-    setContact(contactArray);
+//     contactArray.push(newContact);
+//     console.log('contactArray: ', contactArray);
+//     setContact(contactArray);
     
-}
+// }
+
+
+// let arr =[1,2,3,4,5];
+// function createDivForItems(arr) {
+//     for(let i = 0; i <arr.length; i++) {
+//     const div = document.createElement("div");
+//     div.innerText = arr[i];
+//     document.getElementById("contacts").appendChild(div);
+//     }
+// }
+
+
